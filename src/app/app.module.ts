51fa31ea9727cb273ed83419cb2ea36a03c0a3f8
sathish-programmer <sathishkumarksk007@gmail.com>
2023-06-05 17:14:01 +0530

    added popup routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { OtpEmailComponent } from './auth/email-verification/otp-email/otp-email.component';
-
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { SignupPopupComponent } from './auth/signup-popup/signup-popup.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
-  declarations: [AppComponent, SignupComponent, OtpEmailComponent],
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    OtpEmailComponent,
+    SignupPopupComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,6 +27,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
     CollapseModule.forRoot(),
     AlertModule.forRoot(),
     TooltipModule.forRoot(),
+    ModalModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
