a9a2536e512343ece48de5efc0da25f93e658cf7
Bhavani-RTSG <bhavani.mut@finforz.com>
2023-06-06 10:45:38 +0530

    popup added

--- src/app/auth/signup-popup/modal-content.component.ts
+++ src/app/auth/signup-popup/modal-content.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-modal-content',
   templateUrl: 'modal-content.component.html',
+  styleUrls: ['./modal-content.component.scss'],
 })
 export class ModalContentComponent {
-  closeModal() {
-    // Close modal logic goes here
+  modalRef!: BsModalRef;
+  @Output() onHide = new EventEmitter<void>();
+  constructor(private router: Router,private modalService: BsModalService,private bsModalRef: BsModalRef){}
+  
+  navigate()
+  {
+    this.doHide();
+    this.router.navigateByUrl('/signUp');
+  }
+  doHide() {
+    this.bsModalRef.hide();
   }
 }
