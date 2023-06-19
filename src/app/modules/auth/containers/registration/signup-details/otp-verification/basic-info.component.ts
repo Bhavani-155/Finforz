1add1ec0818b4db90dec5347a1229aa46c3e01a1
Bhavani-RTSG <bhavani.mut@finforz.com>
2023-06-19 18:30:45 +0530

    signup login page

--- src/app/modules/auth/containers/registration/signup-details/otp-verification/basic-info.component.ts
+++ src/app/modules/auth/containers/registration/signup-details/otp-verification/basic-info.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { StepperService } from 'src/app/modules/auth/services/stepper.service';
 
 @Component({
   selector: 'app-basic-info',
   templateUrl: './basic-info.component.html',
 })
 export class basicInfoComponent implements OnInit {
+  constructor(public stepperService:StepperService){}
     ngOnInit(){
+    }
+    next() {
+      this.stepperService.next(5);
     }
 }
