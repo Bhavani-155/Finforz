6aa81156ff00732d82a87a4afc69b7f1cf89d7e7
Bhavani-RTSG <bhavani.mut@finforz.com>
2023-06-21 13:02:10 +0530

    child route module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from './registration/signup-details/signup.component';
-import { OtpEmailComponent } from './registration/signup-details/otp-verification/otp-email.component';
-import { SignupPopupComponent } from './registration/signup-popup/signup-popup.component';
-import { basicInfoComponent } from './registration/signup-details/otp-verification/basic-info.component';
-import { DocumentHomeComponent } from './registration/signup-details/document-verification/document-home/document-home.component';
+
 const routes: Routes = [
-  { path: '', component: SignupPopupComponent },
-  { path: 'popup', component: SignupPopupComponent },
-  { path: 'signUp', component: SignupComponent },
-  { path: 'email-verify', component: OtpEmailComponent },
-  { path: 'verified', component: basicInfoComponent },
-  { path: 'documents', component: DocumentHomeComponent },
+  {
+  path: '',
+    loadChildren: () => import('./registration/signup-details/basic-details/registration-routing.module').then(m => m.RegisterRoutingModule)}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
