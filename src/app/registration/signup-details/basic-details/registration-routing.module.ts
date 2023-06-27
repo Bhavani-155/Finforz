f8055bce177dfd424630a515ec1a78305dae3a86
sathish-programmer <sathishkumarksk007@gmail.com>
2023-06-27 15:16:26 +0530

    singpass api integration

--- src/app/registration/signup-details/basic-details/registration-routing.module.ts
+++ src/app/registration/signup-details/basic-details/registration-routing.module.ts
@@ -1,15 +1,16 @@
-import { RouterModule, Routes } from "@angular/router";
-import { SignupPopupComponent } from "../../signup-popup/signup-popup.component";
-import { SignupComponent } from "../signup.component";
-import { NgModule } from "@angular/core";
-
+import { RouterModule, Routes } from '@angular/router';
+import { SignupPopupComponent } from '../../signup-popup/signup-popup.component';
+import { SignupComponent } from '../signup.component';
+import { NgModule } from '@angular/core';
+import { SingpassModelComponent } from '../document-verification/singpass-model/singpass-model.component';
 const routes: Routes = [
-    { path: '', component: SignupPopupComponent },
-    { path: 'popup', component: SignupPopupComponent },
-    { path: 'signUp', component: SignupComponent },
-  ];
-  @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
-  })
+  { path: '', component: SignupPopupComponent },
+  { path: 'popup', component: SignupPopupComponent },
+  { path: 'signUp', component: SignupComponent },
+  { path: 'singpass', component: SingpassModelComponent },
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
 export class RegisterRoutingModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  title: 'RTSG',
+  production: true,
+  baseUrl: '',
+  logoUrl: '',
+};
