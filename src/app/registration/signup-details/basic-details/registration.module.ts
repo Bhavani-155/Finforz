5c138e30021c27a7abe1b683d6207033e1f1477b
Bhavani-RTSG <bhavani.mut@finforz.com>
2023-07-14 10:51:47 +0530

    regularity and sign pad module added

--- src/app/registration/signup-details/basic-details/registration.module.ts
+++ src/app/registration/signup-details/basic-details/registration.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RegisterRoutingModule } from "./registration-routing.module";
+import { RegularityInfoComponent } from './regularity-info/regularity-info.component';
+import { SignatureComponent } from './signature/signature.component';
 
 
 @NgModule({
     imports: [RegisterRoutingModule],
-    exports: [RouterModule]
+    exports: [RouterModule,RegularityInfoComponent,SignatureComponent],
+    declarations: [
+      RegularityInfoComponent,
+      SignatureComponent,
+    ]
   })
 export class RegisterModule {}
