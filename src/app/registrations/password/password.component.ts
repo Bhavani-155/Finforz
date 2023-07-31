20d8f91a0a0f20631c10a54b38e9a33b6715a54e
manojmath <say2manoj10@gmail.com>
2023-07-31 12:32:31 +0800

    update referralcode

--- src/app/registrations/password/password.component.ts
+++ src/app/registrations/password/password.component.ts
@@ -4,7 +4,7 @@ import { StepperService } from 'src/app/services/stepper.service';
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
- 
+  styleUrls: ['./password.component.scss'],
 })
 export class PasswordComponent {
   constructor(
