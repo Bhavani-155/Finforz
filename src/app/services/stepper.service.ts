518bf20e955a8d57f843a4c8f9dc085464933fcf
kamalfin147 <kamal.jay@finforz.com>
2023-08-09 10:06:37 +0530

    stepper service - review changes

--- src/app/services/stepper.service.ts
+++ src/app/services/stepper.service.ts
@@ -12,72 +12,87 @@ export class StepperService {
   next(currentStep: any) {
     this.currentStep = currentStep;
     this.barWidth = this.currentStep * 10; // Assuming each step is 10% progress
-    if (this.currentStep === 2) {
-      this.title = 'Account Opening';
-      this.subTitle = 'Verification';
-    }else if(this.currentStep === 3)
-    {
-      this.title = 'Account Opening';
-      this.subTitle = 'Account Registration';
-    }else if(this.currentStep === 4)
-    {
-      this.title = 'Account Opening';
-      this.subTitle = 'Basic Info';
-    }else if(this.currentStep === 5) 
-    {
-      this.title = 'Account Opening';
-      this.subTitle = 'Choose Account Opening Method';
-    }else if(this.currentStep === 6) 
-    {
-      this.title = 'Account Opening';
-      this.subTitle = 'Upload Documents';
-    }else if(this.currentStep === 7) 
-    {
-      this.title = 'Account Opening';
-      this.subTitle = 'Upload Documents';
-    }else if(this.currentStep ===8) 
-    {
-      this.title = 'Account Opening';
-      this.subTitle = 'Personal Information';
-    }else if(this.currentStep === 9) 
-    {
-      this.title = 'Account Opening';
-      this.subTitle = 'Permanent Address & Mailing Address';
+    switch(currentStep) { 
+      case 2: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Verification'; 
+        break; 
+      } 
+      case 3: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Account Registration';
+        break; 
+      }
+      case 4: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Basic Info';
+        break; 
+      } 
+      case 5: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Choose Account Opening Method';
+        break; 
+      }
+      case 6: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Upload Documents';
+        break; 
+      } 
+      case 7: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Upload Documents';
+        break; 
+      }
+      case 8: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Personal Information';
+        break; 
+      } 
+      case 9: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Permanent Address & Mailing Address';
+        break; 
+      }
+      case 10: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Financial Information & Assessment';
+        break; 
+      } 
+      case 11: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Investment Knowledge';
+        break; 
+      }
+      case 12: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Customer Account Review';
+        break; 
+      } 
+      case 13: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Declaration';
+        break; 
+      }
+      case 14: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Bank Details';
+        break; 
+      } 
+      case 15: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Regularity Information';
+        break; 
+      }
+      case 16: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Add Signature';
+        break; 
+      }
+      default: { 
+        this.title = 'Account Opening';
+        this.subTitle = 'Basic Info';
+        break; 
+      } 
     }
-    else if(this.currentStep === 10) 
-     {
-      this.title = 'Account Opening';
-      this.subTitle = 'Financial Information & Assessment';
-    }
-    else if(this.currentStep === 11) 
-    {
-     this.title = 'Account Opening';
-     this.subTitle = 'Investment Knowledge';
-   }
-   else if(this.currentStep === 12) 
-    {
-     this.title = 'Account Opening';
-     this.subTitle = 'Customer Account Review';
-   }
-   else if(this.currentStep === 13) 
-    {
-     this.title = 'Account Opening';
-     this.subTitle = 'Declaration';
-   }
-   else if(this.currentStep === 14) 
-    {
-     this.title = 'Account Opening';
-     this.subTitle = 'Bank Details';
-   }
-   else if(this.currentStep === 15) 
-    {
-     this.title = 'Account Opening';
-     this.subTitle = 'Regularity Information';
-   }
-   else if(this.currentStep === 16) 
-    {
-     this.title = 'Account Opening';
-     this.subTitle = 'Add Signature';
-   }
   }
 }
