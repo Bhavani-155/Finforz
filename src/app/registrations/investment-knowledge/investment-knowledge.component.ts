ba739f0c0fe380ac0da883a853b7a7482d99170a
manojmath <say2manoj10@gmail.com>
2023-08-09 12:27:34 +0800

    model validation

--- src/app/registrations/investment-knowledge/investment-knowledge.component.ts
+++ src/app/registrations/investment-knowledge/investment-knowledge.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StepperService } from 'src/app/services/stepper.service';
+import { InvestmentModel } from './investment.model';
+import { ActivatedRoute } from '@angular/router';
+import { ValidationHelper } from 'src/app/shared/util/validationhelper';
 
 @Component({
   selector: 'app-investment-knowledge',
@@ -7,14 +11,64 @@ import { StepperService } from 'src/app/services/stepper.service';
   styleUrls: ['./investment-knowledge.component.scss']
 })
 export class InvestmentKnowledgeComponent {
+  investmentForm: FormGroup;
+  investmentModel: InvestmentModel
+  formBuilder: any;
+  constructor
+  (private stepperService : StepperService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    ){}
+    // registerForm: FormGroup;
 
-  constructor(private stepperService : StepperService){}
+    // createRegistartionFormGroup() {
+    //   this.registerForm = this.fb.group({
+    //     nric: [
+    //       '',
+    //       [ValidationHelper.onlySpaceValidator, ValidationHelper.required],
+    //     ],
+    //   });
+    // }
+    ngOnInit() {
+      this.investmentModel = new InvestmentModel('', '', '', '', '', '', ); // Initialize the user model with default values
+      this.investmentForm = this.fb.group({
+        degree: [this.investmentModel.degree, Validators.required],
+        qualification: [this.investmentModel.qualification, Validators.required],
+        investmentProducts: [this.investmentModel.investmentProducts, Validators.required],
+        sgxOnline: [this.investmentModel.sgxOnline, Validators.required],
+        previousScore: [this.investmentModel.previousScore, Validators.required],
+        financialInstitutions: [this.investmentModel.financialInstitutions, Validators.required],
+      });
+     // this.createRegistartionFormGroup();
+    this.route.queryParams.subscribe((params) => {
+      if (params['singpass']) {
+        this.stepperService.next(7);
+      }
+    });
+    }
+    
   next()
   {
     this.stepperService.next(12);
   }
-  back()
-  {
-    this.stepperService.next(9);
+  onSubmit() {
+    if (this.investmentForm.valid) {
+      console.log(this.investmentForm.value);
+      this.investmentModel = this.investmentForm.value;
+      console.log(this.investmentModel);
+      this.next();
+    }else {
+      this.markFormGroupAsTouched(this.investmentForm);
+    }
   }
+  markFormGroupAsTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupAsTouched(control);
+      }
+    });
+  }
+
 }
