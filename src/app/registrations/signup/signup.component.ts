2e832027023af3be463e37975b6d490b209ccc22
satz-k <sathish.kum@finforz.com>
2023-08-09 11:43:12 +0530

    show focus if field not  valid

--- src/app/registrations/signup/signup.component.ts
+++ src/app/registrations/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StepperService } from 'src/app/services/stepper.service';
 import { ValidationHelper } from 'src/app/shared/util/validationhelper';
@@ -18,6 +18,13 @@ import { SignupModel } from './signup.model';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('nationality') nationality: ElementRef;
+  @ViewChild('countryOfResidence') countryOfResidence: ElementRef;
+  @ViewChild('refrelCode') refrelCode: ElementRef;
+  @ViewChild('mobile') mobile: ElementRef;
+  @ViewChild('email') email: ElementRef;
+
   signupForm: FormGroup;
   signupModel: SignupModel;
   formBuilder: any;
@@ -31,28 +38,27 @@ export class SignupComponent {
   ) {}
 
   customerId: any = '1234321';
-  // registerForm: FormGroup;
-
-  // createRegistartionFormGroup() {
-  //   this.registerForm = this.fb.group({
-  //     nric: [
-  //       '',
-  //       [ValidationHelper.onlySpaceValidator, ValidationHelper.required],
-  //     ],
-  //   });
-  // }
 
   ngOnInit() {
-    this.signupModel = new SignupModel('','','','','','','','',''); // Initialize the user model with default values
+    this.signupModel = new SignupModel('', '', '', '', '', '', '', '', ''); // Initialize the user model with default values
 
     this.signupForm = this.fb.group({
       name: [this.signupModel.name, Validators.required],
       nationality: [this.signupModel.nationality, Validators.required],
-      countryOfResidence: [this.signupModel.countryOfResidence, Validators.required],
-      residencialStatus: [this.signupModel.residencialStatus, Validators.required],
+      countryOfResidence: [
+        this.signupModel.countryOfResidence,
+        Validators.required,
+      ],
+      residencialStatus: [
+        this.signupModel.residencialStatus,
+        Validators.required,
+      ],
       refrelCode: [this.signupModel.refrelCode, Validators.required],
       mobile: [this.signupModel.mobile, Validators.required],
-      email: [this.signupModel.email, [Validators.required, Validators.email,Validators.email]],
+      email: [
+        this.signupModel.email,
+        [Validators.required, Validators.email, Validators.email],
+      ],
       isUsCitizen: [this.signupModel.isUsCitizen, Validators.required],
       privacyPolicy: [this.signupModel.privacyPolicy, Validators.required],
     });
@@ -75,16 +81,33 @@ export class SignupComponent {
   }
   onSubmit() {
     if (this.signupForm.valid) {
-      console.log(this.signupForm.value);
+      // console.log(this.signupForm.value);
       this.signupModel = this.signupForm.value;
-      console.log(this.signupModel);
+      // console.log(this.signupModel);
       this.next();
-    }else {
+    } else {
       this.markFormGroupAsTouched(this.signupForm);
+      this.focusOnInvalidField();
     }
   }
+  focusOnInvalidField() {
+    if (this.signupForm.get('name').invalid) {
+      this.nameInput.nativeElement.focus();
+    } else if (this.signupForm.get('nationality').invalid) {
+      this.nationality.nativeElement.focus();
+    } else if (this.signupForm.get('countryOfResidence').invalid) {
+      this.countryOfResidence.nativeElement.focus();
+    } else if (this.signupForm.get('refrelCode').invalid) {
+      this.refrelCode.nativeElement.focus();
+    } else if (this.signupForm.get('mobile').invalid) {
+      this.mobile.nativeElement.focus();
+    } else if (this.signupForm.get('email').invalid) {
+      this.email.nativeElement.focus();
+    }
+  }
+
   markFormGroupAsTouched(formGroup: FormGroup) {
-    Object.values(formGroup.controls).forEach(control => {
+    Object.values(formGroup.controls).forEach((control) => {
       control.markAsTouched();
 
       if (control instanceof FormGroup) {
